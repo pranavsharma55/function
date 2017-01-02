e8b2c2566cb24004fabc7eb0026ec22fd0ae7401
pranavsharma55 <pranav.sharma55@gmail.com>
2017-01-02 12:47:56 +0530

    Add files via upload

---
+++ add mulfunctions.js
@@ -0,0 +1,22 @@
+
+var a= 10
+var b= 12
+var c= 20
+function multiply(){
+ var a=1
+ var b=12
+ var c=90
+ var d=a*b*c
+ console.log(a,b,c)
+ console.log('multiply result is')
+ console.log(d)
+}
+multiply();
+ console.log(a,b,c)
+ function add()
+ {
+ 	var d=a+b+c
+ 	console.log('addition result is')
+ 	console.log(d)
+ }
+ add();

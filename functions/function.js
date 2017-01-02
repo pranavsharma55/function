34a9fc14a6691543eeab46ba8273f3343a176a48
pranavsharma55 <pranav.sharma55@gmail.com>
2017-01-02 12:16:30 +0530

    example of functions

---
+++ functions/function.js
@@ -0,0 +1,21 @@
+//Function Definition
+function add(){
+	console.log("function definition")
+}
+add();
+//function Declaration
+
+var add=function(){
+	console.log('function declaration')
+
+}
+add();
+//Immediate Invoked Function
+(function()	{
+	console.log("function invoked")
+})()
+
+
+
+alert('this is the function calling')
+	
